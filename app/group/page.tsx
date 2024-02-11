542d16bcdc51c939081dc66f1a3d2a8ed8d7e9ed
ChibaTomoki <michiamo.t1@gmail.com>
2024-02-11 19:45:28 +0900

    add: グループページ

---
+++ app/group/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Group() {
+  return (
+    <div>
+      <h1>Group Page</h1>
+      <Link href="/">トップページへ</Link>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function Index() {
   return (
     <div>
       <h1>Top Page</h1>
+      <Link href="/group">グループページへ</Link>
     </div>
   );
 }
