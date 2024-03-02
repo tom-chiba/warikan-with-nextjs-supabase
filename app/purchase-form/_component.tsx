86742f7137e311f11cb6d16ea862b9b9078f74e0
ChibaTomoki <michiamo.t1@gmail.com>
2024-03-02 16:10:14 +0900

    add: 動的に変化する項目

--- app/purchase-form/_component.tsx
+++ app/purchase-form/_component.tsx
@@ -1,20 +1,56 @@
 "use client";
 
-import { useId } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { Fragment, useEffect, useId } from "react";
+import { useForm, SubmitHandler, useFieldArray } from "react-hook-form";
+
+const dummyPeople = [
+  { id: 0, name: "太郎" },
+  { id: 1, name: "次郎" },
+  { id: 2, name: "三郎" },
+];
 
 type FormItem = {
   title: string;
   date: string;
   note: string;
+  paidList: ((typeof dummyPeople)[number] & { amount: number })[];
+  toPayList: ((typeof dummyPeople)[number] & { amount: number })[];
 };
 
 export const Form = () => {
   const componentId = useId();
 
-  const { register, handleSubmit } = useForm<FormItem>();
+  const { control, register, handleSubmit } = useForm<FormItem>();
+  const {
+    fields: paidListFields,
+    append: paidListAppend,
+    remove: paidListRemove,
+  } = useFieldArray({
+    control,
+    name: "paidList",
+  });
+  const {
+    fields: toPayListFields,
+    append: toPayListAppend,
+    remove: toPayListRemove,
+  } = useFieldArray({
+    control,
+    name: "toPayList",
+  });
 
   const onSubmit: SubmitHandler<FormItem> = (e) => console.dir(e);
+
+  useEffect(() => {
+    dummyPeople.forEach((x) => {
+      paidListAppend({ ...x, amount: 0 });
+      toPayListAppend({ ...x, amount: 0 });
+    });
+
+    return () => {
+      paidListRemove(dummyPeople.map((_, i) => i));
+      toPayListRemove(dummyPeople.map((_, i) => i));
+    };
+  }, [dummyPeople]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -42,6 +78,38 @@ export const Form = () => {
           className="text-black"
           {...register("note")}
         />
+      </div>
+      <div>
+        <span>支払額</span>
+        <div>
+          {paidListFields.map((field, index) => (
+            <Fragment key={field.id}>
+              <label htmlFor={field.id}>{field.name}</label>
+              <input
+                key={field.id}
+                id={field.id}
+                className="text-black"
+                {...register(`paidList.${index}.amount`)}
+              />
+            </Fragment>
+          ))}
+        </div>
+      </div>
+      <div>
+        <span>割勘金額</span>
+        <div>
+          {toPayListFields.map((field, index) => (
+            <Fragment key={field.id}>
+              <label htmlFor={field.id}>{field.name}</label>
+              <input
+                key={field.id}
+                id={field.id}
+                className="text-black"
+                {...register(`toPayList.${index}.amount`)}
+              />
+            </Fragment>
+          ))}
+        </div>
       </div>
       <button type="submit">追加</button>
     </form>
