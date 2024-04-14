62723ea65f02f5d579568539a0ee1e15afd86dd2
ChibaTomoki <michiamo.t1@gmail.com>
2024-04-14 12:10:52 +0900

    add: 未精算リストと精算済リストページにタブを追加

--- app/settled/page.tsx
+++ app/settled/page.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import { TabList } from "../purchase-form/_components";
 import { Table } from "./_component";
 
 export default function PurchaseForm() {
 	return (
 		<div>
 			<h1>精算済リストページ</h1>
+			<TabList
+				tabItems={[
+					{ label: "入力", href: "/purchase-form" },
+					{ label: "未精算リスト", href: "/unsettled" },
+					{ label: "精算済リスト", href: "settled" },
+				]}
+			/>
 			<Link href="/">トップページへ</Link>
 			<Table />
 		</div>
--- app/unsettled/page.tsx
+++ app/unsettled/page.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import { TabList } from "../purchase-form/_components";
 import { Table } from "./_component";
 
 export default function PurchaseForm() {
 	return (
 		<div>
 			<h1>未精算リストページ</h1>
-			<Link href="/">トップページへ</Link>
+			<TabList
+				tabItems={[
+					{ label: "入力", href: "/purchase-form" },
+					{ label: "未精算リスト", href: "/unsettled" },
+					{ label: "精算済リスト", href: "settled" },
+				]}
+			/>
 			<Table />
 		</div>
 	);
