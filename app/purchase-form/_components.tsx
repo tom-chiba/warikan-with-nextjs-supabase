7fe86cbe258a83c525513147d877cbc77750073b
ChibaTomoki <michiamo.t1@gmail.com>
2024-04-29 19:13:12 +0900

    fix: keyが足りないエラー

--- app/purchase-form/_components.tsx
+++ app/purchase-form/_components.tsx
@@ -56,7 +56,7 @@ export const TabList = ({ tabItems }: TabListProps) => {
 	return (
 		<ul className="flex border-b-2 border-b-blue-300 pb-1">
 			{tabItems.map((x) => (
-				<li className="border w-28 p-1">
+				<li className="border w-28 p-1" key={x.label}>
 					<div className="border-b-2 border-b-blue-300 text-center">
 						<Link className="" href={x.href}>
 							{x.label}
