542d220c1175b6212a5fe22906588e3086bbebe4
ChibaTomoki <michiamo.t1@gmail.com>
2024-10-16 21:36:38 +0900

    change: RHFとShadcnを使ったフォームに切り替え

--- app/(authenticated)/clientComponents.tsx
+++ app/(authenticated)/clientComponents.tsx
@@ -1,56 +1,37 @@
 "use client";
 
-import Input from "@/components/Input";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { Switch } from "@/components/ui/switch";
+import { cn } from "@/lib/utils";
 import { createClient } from "@/utils/supabase/client";
-import { useForm } from "@conform-to/react";
-import { parseWithZod } from "@conform-to/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { mdiAccountMinus, mdiAccountPlus, mdiClose } from "@mdi/js";
 import Icon from "@mdi/react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import {
-	type ForwardedRef,
-	forwardRef,
-	useEffect,
-	useRef,
-	useState,
-} from "react";
-import { useFormState } from "react-dom";
-import { purchaseSchema } from "./components";
-import { createPurchase } from "./serverActions";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
 
-type AmountEntryFieldProps = {
-	id: string;
-	label: string;
-	inputName: string;
-	onChange?: (newValue: string) => void;
-};
-
-const AmountEntryField = forwardRef(
-	(
-		{ id, label, inputName, onChange }: AmountEntryFieldProps,
-		inputRef: ForwardedRef<HTMLInputElement>,
-	) => {
-		return (
-			<div className="flex gap-4 border-b-2 border-gray-300 p-1 min-w-0">
-				<label className="truncate" htmlFor={id}>
-					{label}
-				</label>
-				<div className="flex gap-1">
-					<Input
-						id={id}
-						name={inputName}
-						size="small"
-						onChange={(e) => onChange?.(e.target.value)}
-						ref={inputRef}
-					/>
-					<span>円</span>
-				</div>
-			</div>
-		);
-	},
-);
-
-type PurchasersDialogProps = {
+type ClientPurchasersDialogProps = {
 	initialPurchasers: {
 		id: number;
 		name: string;
@@ -67,7 +48,7 @@ const ClientPurchasersDialog = ({
 	onClose,
 	onCreate,
 	onDelete,
-}: PurchasersDialogProps) => {
+}: ClientPurchasersDialogProps) => {
 	const supabase = createClient();
 	const queryClient = useQueryClient();
 
@@ -288,12 +269,50 @@ type ClientFormProps = {
 	}[];
 };
 
+const purchaseSchema = z.object({
+	title: z.string().min(1, { message: "必須" }),
+	date: z.date().optional(),
+	note: z.string().optional(),
+	purchasersAmountPaid: z.array(
+		z.object({
+			amountPaid: z.union([
+				z
+					.number({ message: "数字じゃないとダメ" })
+					.nonnegative({ message: "0以上の値じゃないとダメ" })
+					.int({ message: "整数じゃないとダメ" }),
+				z.string().length(0, { message: "数字じゃないとダメ" }),
+			]),
+		}),
+	),
+	purchasersAmountToPay: z.array(
+		z.object({
+			amountToPay: z.union([
+				z
+					.number({ message: "数字じゃないとダメ" })
+					.nonnegative({ message: "0以上の値じゃないとダメ" })
+					.int({ message: "整数じゃないとダメ" }),
+				z.string().length(0, { message: "数字じゃないとダメ" }),
+			]),
+		}),
+	),
+});
+
 export const ClientForm = ({ initialPurchasers }: ClientFormProps) => {
 	const supabase = createClient();
 	const queryClient = useQueryClient();
 
 	const [clientPurchasersDialogIsOpen, setClientPurchasersDialogIsOpen] =
 		useState(false);
+	const [equallyDivideCheckIsChecked, setEquallyDivideCheckIsChecked] =
+		useState(false);
+
+	type DistributivePick<T, K extends keyof T> = T extends unknown
+		? Pick<T, K>
+		: never;
+	type UseQueryDataAndStatus<T> = DistributivePick<
+		ReturnType<typeof useQuery<T>>,
+		"data" | "status" | "isRefetching"
+	>;
 
 	const purchasersCache = useQuery({
 		queryKey: ["purchasers"],
@@ -308,178 +327,319 @@ export const ClientForm = ({ initialPurchasers }: ClientFormProps) => {
 		},
 		initialData: initialPurchasers,
 	});
+	const createPurchase = useMutation({
+		mutationFn: async ({
+			title,
+			date,
+			note,
+			purchasersAmountPaid,
+			purchasersAmountToPay,
+		}: z.infer<typeof purchaseSchema>) => {
+			const { data: purchaseData, error: purchaseError } = await supabase
+				.from("purchases")
+				.insert([
+					{
+						title: title,
+						purchase_date: date?.toLocaleDateString("sv-SE") || null, // NOTE: 参考記事→https://www.ey-office.com/blog_archive/2023/04/18/short-code-to-get-todays-date-in-yyyy-mm-dd-format-in-javascript/
+						note: note ?? "",
+					},
+				])
+				.select();
+			if (purchaseError) throw new Error(purchaseError.message);
 
-	const createPurchaseWithPurchasers = createPurchase.bind(
-		null,
-		purchasersCache.data.map((x) => x.id),
-	);
+			const insertedPurchaseData = purchaseData[0];
 
-	const [lastResult, action] = useFormState(
-		createPurchaseWithPurchasers,
-		undefined,
-	);
-	const [form, fields] = useForm({
-		lastResult,
-		onValidate({ formData }) {
-			return parseWithZod(formData, { schema: purchaseSchema });
+			const { error: purchasersPurchasesError } = await supabase
+				.from("purchasers_purchases")
+				.insert(
+					purchasersAmountPaid.map((x, i) => ({
+						purchase_id: insertedPurchaseData.id,
+						purchaser_id: purchasersCache.data[i].id,
+						amount_paid: typeof x.amountPaid === "number" ? x.amountPaid : null,
+						amount_to_pay:
+							typeof purchasersAmountToPay[i].amountToPay === "number"
+								? purchasersAmountToPay[i].amountToPay
+								: null,
+					})),
+				)
+				.select();
+
+			// TODO: トランザクション処理
+			if (purchasersPurchasesError)
+				throw new Error(
+					`処理に失敗しました。purchases tableからid=${insertedPurchaseData.id}に紐づく行を削除してください。`,
+				);
 		},
-		shouldValidate: "onBlur",
-		defaultValue: {
-			purchasers: initialPurchasers.map((x) => ({ name: x.name })),
+		onSuccess: () => form.reset(),
+		throwOnError: true,
+	});
+
+	const purchaserNames: UseQueryDataAndStatus<string[]> =
+		purchasersCache.status !== "success"
+			? {
+					status: purchasersCache.status,
+					data: undefined,
+					isRefetching: purchasersCache.isRefetching,
+				}
+			: {
+					status: "success",
+					data: purchasersCache.data?.map((x) => x.name),
+					isRefetching: purchasersCache.isRefetching,
+				};
+
+	const form = useForm<z.infer<typeof purchaseSchema>>({
+		resolver: zodResolver(purchaseSchema),
+		defaultValues: {
+			title: "",
+			date: new Date(),
+			note: "",
+			purchasersAmountPaid:
+				purchaserNames.data?.map((x) => ({ amountPaid: 0 })) ?? [],
+			purchasersAmountToPay:
+				purchaserNames.data?.map((x) => ({ amountToPay: 0 })) ?? [],
 		},
 	});
-	const purchasersFieldList = fields.purchasers.getFieldList();
 
-	const amountPaidMapRefs = useRef<Map<string, HTMLInputElement> | null>(null);
-	const amountToPayMapRefs = useRef<Map<string, HTMLInputElement> | null>(null);
-	const getAmountPaidMap = () => {
-		if (!amountPaidMapRefs.current) amountPaidMapRefs.current = new Map();
+	const { fields: purchasersAmountPaidFields } = useFieldArray({
+		control: form.control,
+		name: "purchasersAmountPaid",
+	});
+	const {
+		fields: purchasersAmountToPayFields,
+		replace: purchasersAmountToPayReplace,
+	} = useFieldArray({
+		control: form.control,
+		name: "purchasersAmountToPay",
+	});
 
-		return amountPaidMapRefs.current;
-	};
-	const getAmountToPayMap = () => {
-		if (!amountToPayMapRefs.current) amountToPayMapRefs.current = new Map();
-
-		return amountToPayMapRefs.current;
-	};
-
-	const equallyDivideCheckRef = useRef<HTMLInputElement>(null);
-
-	const divideEqually = () => {
-		const amountPaidMap = getAmountPaidMap();
-		const amountToPayMap = getAmountToPayMap();
-
-		let amountPaidSum = 0;
-		for (const amountPaidRef of Array.from(amountPaidMap.values()))
-			amountPaidSum += Number(amountPaidRef.value);
-
-		for (const amountToPayRef of Array.from(amountToPayMap.values()))
-			amountToPayRef.value = String(
-				amountPaidSum / purchasersCache.data.length,
-			);
+	const handleSubmit = (values: z.infer<typeof purchaseSchema>) => {
+		createPurchase.mutate(values);
 	};
 
-	useEffect(() => {
-		form.errors && alert(form.errors);
-	}, [form.errors]);
+	const calculateAmountToPay = (amountPaidSum: number) => {
+		const dividedEquallyPurchasersAmountToPay = watchedPurchasersAmountPaid.map(
+			(x) => ({
+				amountToPay: amountPaidSum / watchedPurchasersAmountPaid.length,
+			}),
+		);
 
-	useEffect(() => {
-		if (fields.purchasers.getFieldList().length === purchasersCache.data.length)
-			return;
+		purchasersAmountToPayReplace(dividedEquallyPurchasersAmountToPay);
+	};
 
-		for (const _ of fields.purchasers.getFieldList()) {
-			form.remove({ name: fields.purchasers.name, index: 0 });
-		}
-		purchasersCache.data.forEach((x, index) => {
-			form.insert({
-				name: fields.purchasers.name,
-				index,
-				defaultValue: { name: x.name },
-			});
-		});
-	}, [fields.purchasers, form, purchasersCache.data]);
+	const watchedPurchasersAmountPaid = form.watch("purchasersAmountPaid");
 
 	return (
-		<>
-			<form id={form.id} action={action} onSubmit={form.onSubmit} noValidate>
-				<div>
-					<label htmlFor={fields.title.id}>購入品名</label>
-					<Input id={fields.title.id} name={fields.title.name} />
-					<p>{fields.title.errors}</p>
-				</div>
-				<div>
-					<label htmlFor={fields.date.id}>購入日</label>
-					<Input id={fields.date.id} type="date" name={fields.date.name} />
-					<p>{fields.date.errors}</p>
-				</div>
-				<div>
-					<label htmlFor={fields.note.id}>メモ</label>
-					<Input id={fields.note.id} name={fields.note.name} />
-					<p>{fields.note.errors}</p>
-				</div>
-				<button
-					className="border"
-					type="button"
-					onClick={() => setClientPurchasersDialogIsOpen(true)}
-				>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="title"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>購入品名</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="date"
+					render={({ field }) => (
+						<FormItem className="flex flex-col">
+							<FormLabel>購入日</FormLabel>
+							<Popover>
+								<PopoverTrigger asChild>
+									<FormControl>
+										<Button
+											variant={"outline"}
+											className={cn(
+												"w-[240px] pl-3 text-left font-normal",
+												!field.value && "text-muted-foreground",
+											)}
+										>
+											{field.value ? (
+												format(field.value, "PPP")
+											) : (
+												<span>Pick a date</span>
+											)}
+											<CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+										</Button>
+									</FormControl>
+								</PopoverTrigger>
+								<PopoverContent className="w-auto p-0" align="start">
+									<Calendar
+										mode="single"
+										selected={field.value}
+										onSelect={field.onChange}
+										disabled={(date) =>
+											date > new Date() || date < new Date("1900-01-01")
+										}
+										initialFocus
+									/>
+								</PopoverContent>
+							</Popover>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="note"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>メモ</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<Button onClick={() => setClientPurchasersDialogIsOpen(true)}>
 					メンバー管理
-				</button>
-				<div>
-					<span>支払額</span>
-					<div>
-						{purchasersFieldList.map((x) => {
-							const fieldSet = x.getFieldset();
+				</Button>
 
-							return (
-								<div key={fieldSet.amountPaid.key} className="flex justify-end">
-									<AmountEntryField
-										id={fieldSet.amountPaid.id}
-										label={fieldSet.name.initialValue ?? ""}
-										inputName={fieldSet.amountPaid.name}
-										ref={(node) => {
-											const amountPaidMap = getAmountPaidMap();
-											if (node) {
-												amountPaidMap.set(fieldSet.amountPaid.id, node);
-											} else {
-												amountPaidMap.delete(fieldSet.amountPaid.id);
-											}
-										}}
-										onChange={() => {
-											if (!equallyDivideCheckRef.current?.checked) return;
+				{purchaserNames.status === "error" ? (
+					<p>error</p>
+				) : (
+					<>
+						<Card>
+							<CardHeader>
+								<CardTitle>支払額</CardTitle>
+							</CardHeader>
+							<CardContent>
+								<ul>
+									{purchasersAmountPaidFields.map((item, index) => (
+										<li key={item.id}>
+											<FormField
+												control={form.control}
+												name={`purchasersAmountPaid.${index}.amountPaid`}
+												render={({ field }) => (
+													<FormItem>
+														<FormLabel>{purchaserNames.data[index]}</FormLabel>
+														<FormControl>
+															<Input
+																{...field}
+																onChange={(e) => {
+																	const inputValue = e.target.value;
+																	const inputValueAsNumber = Number(inputValue);
+																	if (
+																		!inputValue ||
+																		Number.isNaN(inputValueAsNumber)
+																	)
+																		field.onChange(inputValue);
+																	else field.onChange(inputValueAsNumber);
 
-											divideEqually();
-										}}
-									/>
-									<p>{fieldSet.amountPaid.errors}</p>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-				<div>
-					<span>割勘金額</span>
-					<div>
-						<input
-							ref={equallyDivideCheckRef}
-							type="checkbox"
-							id="equallyDivideCheck"
-							onChange={() => {
-								if (!equallyDivideCheckRef.current?.checked) return;
+																	if (!equallyDivideCheckIsChecked) return;
 
-								divideEqually();
-							}}
-						/>
-						<label htmlFor="equallyDivideCheck">等分</label>
-					</div>
-					<div>
-						{purchasersFieldList.map((x) => {
-							const fieldSet = x.getFieldset();
+																	const amountPaidSum =
+																		watchedPurchasersAmountPaid.reduce(
+																			(
+																				accumulator,
+																				currentValue,
+																				currentIndex,
+																			) => {
+																				if (currentIndex === index)
+																					return (
+																						accumulator +
+																						Number(e.target.value ?? 0)
+																					);
+																				return (
+																					accumulator +
+																					Number(currentValue.amountPaid ?? 0)
+																				);
+																			},
+																			0,
+																		);
 
-							return (
-								<div
-									key={fieldSet.amountToPay.key}
-									className="flex justify-end"
-								>
-									<AmountEntryField
-										id={fieldSet.amountToPay.id}
-										label={fieldSet.name.initialValue ?? ""}
-										inputName={fieldSet.amountToPay.name}
-										ref={(node) => {
-											const amountToPayMap = getAmountToPayMap();
-											if (node) {
-												amountToPayMap.set(fieldSet.amountToPay.id, node);
-											} else {
-												amountToPayMap.delete(fieldSet.amountToPay.id);
-											}
-										}}
-									/>
-									<p>{fieldSet.amountToPay.errors}</p>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-				<button type="submit">追加</button>
+																	calculateAmountToPay(amountPaidSum);
+																}}
+																placeholder="0"
+																inputMode="numeric"
+															/>
+														</FormControl>
+														<FormMessage />
+													</FormItem>
+												)}
+											/>
+										</li>
+									))}
+								</ul>
+							</CardContent>
+						</Card>
+
+						<Card>
+							<CardHeader>
+								<CardTitle>割勘金額</CardTitle>
+							</CardHeader>
+							<CardContent>
+								<Switch
+									id="equallyDivideCheck"
+									onCheckedChange={() => {
+										if (equallyDivideCheckIsChecked) return;
+
+										const amountPaidSum = watchedPurchasersAmountPaid.reduce(
+											(accumulator, currentValue) => {
+												return (
+													accumulator + Number(currentValue.amountPaid ?? 0)
+												);
+											},
+											0,
+										);
+
+										calculateAmountToPay(amountPaidSum);
+										setEquallyDivideCheckIsChecked((prev) => !prev);
+									}}
+									checked={equallyDivideCheckIsChecked}
+								/>
+								<Label htmlFor="equallyDivideCheck">等分</Label>
+
+								<ul>
+									{purchasersAmountToPayFields.map((item, index) => (
+										<li key={item.id}>
+											<FormField
+												control={form.control}
+												name={`purchasersAmountToPay.${index}.amountToPay`}
+												render={({ field }) => (
+													<FormItem>
+														<FormLabel>{purchaserNames.data[index]}</FormLabel>
+														<FormControl>
+															<Input
+																{...field}
+																onChange={(e) => {
+																	const inputValue = e.target.value;
+																	const inputValueAsNumber = Number(inputValue);
+																	if (
+																		!inputValue ||
+																		Number.isNaN(inputValueAsNumber)
+																	)
+																		field.onChange(e);
+																	else field.onChange(inputValueAsNumber);
+																}}
+																placeholder="0"
+																inputMode="numeric"
+															/>
+														</FormControl>
+														<FormMessage />
+													</FormItem>
+												)}
+											/>
+										</li>
+									))}
+								</ul>
+							</CardContent>
+						</Card>
+					</>
+				)}
+
+				<Button type="submit">追加</Button>
+
 				<ClientPurchasersDialog
 					isOpen={clientPurchasersDialogIsOpen}
 					onClose={() => setClientPurchasersDialogIsOpen(false)}
@@ -492,6 +652,6 @@ export const ClientForm = ({ initialPurchasers }: ClientFormProps) => {
 					}}
 				/>
 			</form>
-		</>
+		</Form>
 	);
 };
