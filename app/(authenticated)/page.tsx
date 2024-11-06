d064e0e4268d9e8d3a607014535b5476726f187a
ChibaTomoki <michiamo.t1@gmail.com>
2024-11-06 22:56:01 +0900

    change: スマホでもタブが全て表示できるようにタイトルを変更

--- app/(authenticated)/page.tsx
+++ app/(authenticated)/page.tsx
@@ -10,13 +10,13 @@ export default function Index() {
 					<Link href="/">入力</Link>
 				</TabsTrigger>
 				<TabsTrigger value="unsettled" asChild>
-					<Link href="/unsettled">未精算リスト</Link>
+					<Link href="/unsettled">未精算</Link>
 				</TabsTrigger>
 				<TabsTrigger value="settled" asChild>
-					<Link href="/settled">精算済リスト</Link>
+					<Link href="/settled">精算済</Link>
 				</TabsTrigger>
 				<TabsTrigger value="member" asChild>
-					<Link href="/member">メンバー管理</Link>
+					<Link href="/member">メンバー</Link>
 				</TabsTrigger>
 			</TabsList>
 			<TabsContent value="purchaseForm">
